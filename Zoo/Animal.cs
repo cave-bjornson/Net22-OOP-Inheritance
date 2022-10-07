6b93120102e3a0607b3d9162b293db2040a3dc55
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-10-08 01:16:33 +0200

    feat: add Animal class
    Adds the base class for animals.
    
    closes #1

---
+++ Zoo/Animal.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------
+// <copyright file="Animal.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Animal type.
+// </summary>
+// --------------------------------------------------------------------------------
+
+namespace Zoo
+{
+    public class Animal
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public Sex Sex { get; set; } = Sex.TBD;
+
+        public bool Alive { get; private set; } = true;
+
+        public bool Hungry { get; private set; } = false;
+
+        public bool Tired { get; private set; } = false;
+
+        public void Eat(object food)
+        {
+            Hungry = false;
+        }
+
+        public void Sleep()
+        {
+            Tired = false;
+        }
+
+        public void Move()
+        {
+            Hungry = true;
+            Tired = true;
+        }
+
+        public virtual Animal? Mate(Animal otherAnimal)
+        {
+            if (this.GetType() == otherAnimal.GetType() && this.Sex != otherAnimal.Sex)
+            {
+                return new Animal();
+            }
+
+            return null;
+        }
+    }
+}
