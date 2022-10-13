d386389631315d76810265446337377f4bb6fa39
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-10-13 23:50:48 +0200

    feat: add 3 inherited classes
    Adds: 2 concrete classes, Bear and Bird, and an abstract class Hominid for Humans and Great Apes.

---
+++ Zoo/Hominid.cs
@@ -0,0 +1,18 @@
+﻿namespace Zoo;
+
+using System.Collections;
+public abstract class Hominid : Animal
+{
+    private readonly ICollection? inventory;
+
+    protected Hominid()
+    {
+    }
+
+    protected Hominid(string species, string name, Gender gender)
+        : base(species, name, gender)
+    {
+    }
+
+    protected abstract void Speak();
+}
