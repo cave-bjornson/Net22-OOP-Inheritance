fe3f031a4c84324c75af1550d62861371ebda573
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-10-16 15:59:56 +0200

    feat!: add static class for speech
    Adds a static method that accesses the SAM.exe.

---
+++ SamSpeaker/Speaker.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace SamSpeaker
+{
+    public static class Speaker
+    {
+        public static void Speak(string speechLine)
+        {
+            Process speaker = new Process();
+            speaker.StartInfo.FileName = "sam.exe";
+            speaker.StartInfo.Arguments = speechLine;
+            speaker.Start();
+        }
+    }
+}
